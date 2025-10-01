c8569181f2ae7d22d4cc255672dedd5f61589186
IR007-jpg <irvanr0071@gmail.com>
2025-10-01 18:31:53 +0700

    russian roulette 10/1/25

--- public/pages/games/russian_roulette.php
+++ public/pages/games/russian_roulette.php
@@ -11,8 +11,10 @@
 
     <div class="arena">
       <div class="card player-card">
-        <div class="avatar player-avatar"></div>
-        <div class="player-name">PLAYER</div>
+        <div class="avatar player-avatar">
+          <img src="../../assets/img/photo_profile/1759201128_kitsan.jpg" alt="" style="width:100px; height:100px;">
+        </div>
+        <div class="player-name">Hiiragi</div>
         <div class="health-bar">
           <div class="health-fill" id="playerHealth"></div>
         </div>
